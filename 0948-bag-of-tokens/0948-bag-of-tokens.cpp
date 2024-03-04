ece8d996ef75c4437c75130adaa774d5a319f0b1
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-04 09:26:34 +0530

    Time: 4 ms (74.87%), Space: 13 MB (55.08%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        // int score = 0;
-        // int res = 0;
          sort(tokens.begin(),tokens.end());
-        // if(power < tokens[0]){
-        //     return score;
-        // }
-        // int sum = 0;
-        // int n = tokens.size();
-        // for(int i=0,j=n-1; i<n && j>=0 && i<=j ;){
-        //     cout<<" Hello"<<" ";
-        //     if(power>=tokens[i]){
-        //         power-=tokens[i++];
-        //         score++;
-        //     }
-        //     else if(score!=0){
-        //         power+=tokens[j--];
-        //         score--;
-        //     }
-        //     res = max(res,score);
-        //     cout<<power<<" "<<score<<endl;
-        // }
         int p=power;
         if(tokens.size()==0 || p<tokens[0])
             return 0;
