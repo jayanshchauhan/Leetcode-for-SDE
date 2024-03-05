fd1ab643a4765b043d46d8bbe481d37c34e750a7
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-06 01:02:21 +0530

    Time: 4 ms (48.46%), Space: 7.9 MB (42.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        // for(int i=0;i<t.length();i++){
-        //     if(s.find(t[i]) == string::npos){
-        //         return char(t[i]);
-        //     }
-        // }
-        // return 0;
-    //     s+=t; int c=0;
-    // for(auto x: s)
-    // {
-    //     c^=x;
-    // }
-    // return c;
+       
         for(int i=0;i<s.size();i++)
 		t[i+1]+=t[i]-s[i]; //Passing the diff: (t[i]-s[i]) to t[i+1]
       return t[t.size()-1]; //The diff will be carried over to the last element eventually
