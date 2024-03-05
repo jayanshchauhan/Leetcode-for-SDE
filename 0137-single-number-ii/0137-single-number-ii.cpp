f676d36259621259f9cdc17ff623aaee158b1967
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-06 00:28:14 +0530

    Time: 11 ms (15.39%), Space: 11.7 MB (63.90%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unsigned int ans = 0;
+        unsigned int shift = 1;
+        for(int i=0;i<32;i++){
+             int count = 0;
+            for(auto ele:nums){
+                if(ele&shift){
+                    count+=1;
+                }
+            }
+            if(count%3!=0){
+                ans+=shift;
+            }
+            shift *=2;
+        }
+        return ans;
+    }
+};
