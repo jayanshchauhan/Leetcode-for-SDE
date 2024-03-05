a3f81ec5ef34c0c8d08009babbf35a779bef6e69
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-06 01:02:08 +0530

    Time: 5 ms (29.67%), Space: 7.8 MB (65.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -7,11 +7,14 @@ public:
         //     }
         // }
         // return 0;
-        s+=t; int c=0;
-    for(auto x: s)
-    {
-        c^=x;
-    }
-    return c;
+    //     s+=t; int c=0;
+    // for(auto x: s)
+    // {
+    //     c^=x;
+    // }
+    // return c;
+        for(int i=0;i<s.size();i++)
+		t[i+1]+=t[i]-s[i]; //Passing the diff: (t[i]-s[i]) to t[i+1]
+      return t[t.size()-1]; //The diff will be carried over to the last element eventually
     }
 };
