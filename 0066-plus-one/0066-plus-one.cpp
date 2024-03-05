40968de303aa4bc0f59e8f0633eb910d042a8515
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-05 21:32:48 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (11.61%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,37 +2,49 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size()-1;
-        int flag = 0;
-        vector<int> v;
-        int i = n;
-        bool pre=false,skip=false;
-        if(digits[n] != 9){
-            for(int i=0;i<n;i++){
-                v.push_back(digits[i]);
-            }
-            v.push_back(digits[n]+1);
-        }else{
-            for(int i=n;i>=0;i--){
-                if(digits[i] == 9 && skip != true){
-                    v.push_back(0);
-                    pre = true;
-                }else if(pre == true){
-                    v.push_back(digits[i] + 1);
-                    pre = false;
-                    skip = true;
-                }else{
-                    v.push_back(digits[i]);
-                    pre = false;
-                }
+//         int n = digits.size()-1;
+//         int flag = 0;
+//         vector<int> v;
+//         int i = n;
+//         bool pre=false,skip=false;
+//         if(digits[n] != 9){
+//             for(int i=0;i<n;i++){
+//                 v.push_back(digits[i]);
+//             }
+//             v.push_back(digits[n]+1);
+//         }else{
+//             for(int i=n;i>=0;i--){
+//                 if(digits[i] == 9 && skip != true){
+//                     v.push_back(0);
+//                     pre = true;
+//                 }else if(pre == true){
+//                     v.push_back(digits[i] + 1);
+//                     pre = false;
+//                     skip = true;
+//                 }else{
+//                     v.push_back(digits[i]);
+//                     pre = false;
+//                 }
                 
-            }
-            if(pre == true){
-                    v.push_back(1);
-                }
-                reverse(v.begin(),v.end());
-        }
+//             }
+//             if(pre == true){
+//                     v.push_back(1);
+//                 }
+//                 reverse(v.begin(),v.end());
+//         }
         
-        return v;
+//         return v;
+        int n = digits.size() ;
+        for ( int i = n - 1 ; i  >= 0 ; i -- ) {
+            if ( digits[i]  <  9) {
+                digits[i] ++ ;
+                return digits; 
+            }else {
+                digits[i] = 0  ;
+            }
+
+        }
+        digits.insert(digits.begin(), 1 ) ; 
+        return digits;
     }
 };
