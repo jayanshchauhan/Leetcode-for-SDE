e439aecb03a7bbcb78d2b8dc384dde50544e2ff9
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-09 09:45:06 +0530

    Time: 9 ms (12.16%), Space: 13.1 MB (38.83%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        int n = nums1.size();
-        int m = nums2.size();
-        int i = 0,j = 0;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        while(i<n && j<m){
-            if(nums1[i] > nums2[j]){
-                j++;
-            }else if(nums1[i] < nums2[j]){
-                i++;
-            }else{
-                res.push_back(nums1[i]);
-                i++;
-                j++;
+        // vector<int> res;
+        // int n = nums1.size();
+        // int m = nums2.size();
+        // int i = 0,j = 0;
+        // sort(nums1.begin(),nums1.end());
+        // sort(nums2.begin(),nums2.end());
+        // while(i<n && j<m){
+        //     if(nums1[i] > nums2[j]){
+        //         j++;
+        //     }else if(nums1[i] < nums2[j]){
+        //         i++;
+        //     }else{
+        //         res.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // return res;
+        map<int,int>mpp1;
+
+        for(int i=0;i<nums1.size();i++){
+            mpp1[nums1[i]]++;
+        }
+        
+        vector<int>ans;
+
+        for(int i=0;i<nums2.size();i++){
+            if(mpp1[nums2[i]]){
+                ans.push_back(nums2[i]);
+                mpp1[nums2[i]]--;
             }
         }
-        return res;
+
+        return ans;
     }
 };
