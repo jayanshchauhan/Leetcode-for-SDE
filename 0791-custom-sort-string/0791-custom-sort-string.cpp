d4f73e4c64c1545ba3e4049414e95d183e403295
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-11 09:30:10 +0530

    Time: 3 ms (50.27%), Space: 9.7 MB (6.35%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        unordered_map<char,int> um;
-        string res = "";
+//         unordered_map<char,int> um;
+//         string res = "";
         
-        for(int i=0;i<s.length();i++){ 
-            um[s[i]]++;
-        }
+//         for(int i=0;i<s.length();i++){ 
+//             um[s[i]]++;
+//         }
         
-        for(int i=0;i<order.length();i++){
-            if(s.find(order[i]) != string::npos){
-                while(um[order[i]]--){
-                    res+=order[i];
-                }
-            }
-        }
-        for(int i=0;i<s.length();i++){ 
-            if(um[s[i]]!=-1){
-                res+=s[i];
-            }
+//         for(int i=0;i<order.length();i++){
+//             if(s.find(order[i]) != string::npos){
+//                 while(um[order[i]]--){
+//                     res+=order[i];
+//                 }
+//             }
+//         }
+//         for(int i=0;i<s.length();i++){ 
+//             if(um[s[i]]!=-1){
+//                 res+=s[i];
+//             }
             
-        }
-        return res;
+//         }
+//         return res;
+         map<char,int> mp;
+        for(int i=0;i<order.length();i++){
+            mp[order[i]]=i;
+        }   
+        
+        auto compare = [&](char a, char b) { return mp[a] < mp[b]; }; 
+        
+        sort(s.begin(),s.end(),compare);
+        return s;
     }
 };
