8f907a772d9718cb46ed854e4dde2c3be8929f30
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-03-13 09:06:18 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int total = n*(n+1)/2;
-        vector<int> psum(n+1);
-        psum[0] = 0;
-        for(int i=1;i<=n;i++){
-            psum[i] = psum[i-1] + i;
+        // int total = n*(n+1)/2;
+        // vector<int> psum(n+1);
+        // psum[0] = 0;
+        // for(int i=1;i<=n;i++){
+        //     psum[i] = psum[i-1] + i;
+        // }
+        // for(int i=0;i<psum.size();i++){
+        //     if(psum[i] == total - psum[i] + i){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        if(n == 1){
+            return n;
         }
-        for(int i=0;i<psum.size();i++){
-            if(psum[i] == total - psum[i] + i){
-                return i;
+        int start = 1;
+        int end = n;
+        int mid = start + (end - start)/2;
+        while(start < end){
+            int sum1 = (mid * (mid + 1)/2);
+            int sum2 = ((n * (n + 1))/2) - (mid * (mid + 1)/2) + mid;
+           
+            if(sum1 == sum2){
+                return mid;
             }
+            else if(sum2 > sum1){
+             start = mid + 1;
+            }
+            else{
+                end = mid -1;
+            }
+           
+           
+            mid = start + (end - start)/2;
+            
+            
+            
         }
         return -1;
     }
