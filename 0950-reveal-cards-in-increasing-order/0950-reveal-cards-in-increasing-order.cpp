3cc05e42907ad50fac54a614bac748b8662f7ddc
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-04-10 08:48:43 +0530

    Time: 6 ms (30.99%), Space: 10.7 MB (91.52%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -1,28 +1,59 @@
+// class Solution {
+// public:
+//     vector<int> deckRevealedIncreasing(vector<int>& deck) {
+//         int N = deck.size();
+//         queue<int> queue;
+
+//         // Create a queue of indexes
+//         for (int i = 0; i < N; i++) {
+//             queue.push(i);
+//         }
+        
+//         sort(deck.begin(), deck.end());
+
+//         // Put cards at correct index in result
+//         vector<int> result(N);
+//         for (int i = 0; i < N; i++) {
+//             // Reveal Card
+//             result[queue.front()] = deck[i];
+//             queue.pop();
+
+//             // Move next card to bottom
+//             if (!queue.empty()) {
+//                 queue.push(queue.front());
+//                 queue.pop();
+//             }
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         int N = deck.size();
-        queue<int> queue;
+        vector<int> result(N);
+        bool skip = false;
+        int indexInDeck = 0;
+        int indexInResult = 0;
 
-        // Create a queue of indexes
-        for (int i = 0; i < N; i++) {
-            queue.push(i);
-        }
-        
         sort(deck.begin(), deck.end());
 
-        // Put cards at correct index in result
-        vector<int> result(N);
-        for (int i = 0; i < N; i++) {
-            // Reveal Card
-            result[queue.front()] = deck[i];
-            queue.pop();
+        while (indexInDeck < N) {
+            // There is an available gap in result
+            if (result[indexInResult] == 0) {
 
-            // Move next card to bottom
-            if (!queue.empty()) {
-                queue.push(queue.front());
-                queue.pop();
+                // Add a card to result
+                if (!skip) {
+                    result[indexInResult] = deck[indexInDeck];
+                    indexInDeck++;
+                }
+
+                // Toggle skip to alternate between adding and skipping cards
+                skip = !skip;
             }
+            // Progress to the next index of result array
+            indexInResult = (indexInResult + 1) % N;
         }
         return result;
     }
