06ffc6c5d94f0810d63f95fe01d7d13d42d76682
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2024-05-05 09:16:32 +0530

    Time: 10 ms (6.56%), Space: 11.4 MB (43.54%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        // Overwrite data of next node on current node.
+        node->val = node->next->val;
+        // Make current node point to next of next node.
+        node->next = node->next->next;
+    }
+};
