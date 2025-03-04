6afba0d0f05007f83a174c0bbd61a657e2ce539f
Jayansh Chauhan <54074798+jayanshchauhan@users.noreply.github.com>
2025-03-04 07:56:49 +0530

    Time: 0 ms (100%), Space: 5.8 MB (100%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        int power = 0;
-
-        // Find the largest power that is smaller or equal to n
-        while (pow(3, power) <= n) {
-            power++;
-        }
-
-        while (n > 0) {
-            // Add current power to the sum
-            if (n >= pow(3, power)) n -= pow(3, power);
-            // We cannot use the same power twice
-            if (n >= pow(3, power)) return false;
-            // Move to the next power
-            power--;
-        }
-
-        // n has reached 0
-        return true;
+        if(n==0)
+            return true;
+        return checkPowersOfThree(n/3) && (n%3 == 0 || n%3 == 1);
     }
 };
